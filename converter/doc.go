e3307e006a201bff26cf645c5941c011517f0cb7
Kevin McDonald <github@sudorandom.com>
2024-09-22 15:18:14 +0200

    add examples

---
+++ converter/doc.go
@@ -0,0 +1,4 @@
+// protoc-gen-connect-openapi is a tool for generating OpenAPIv3.1 specs from protobuf. These specs
+// are specific to the Connect protocol.
+// See the github repo for more information: https://github.com/sudorandom/protoc-gen-connect-openapi).
+package converter
