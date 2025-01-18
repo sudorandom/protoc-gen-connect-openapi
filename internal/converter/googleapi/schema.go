c005b3d590bb5f6bf881e3cd4d29dd15e7c5985e
Kevin McDonald <github@sudorandom.com>
2025-01-18 07:36:10 +0100

    fix: test field_behavior annotations

--- internal/converter/googleapi/schema.go
+++ internal/converter/googleapi/schema.go
@@ -13,33 +13,35 @@ func SchemaWithPropertyAnnotations(schema *base.Schema, desc protoreflect.FieldD
 	if !proto.HasExtension(dopts, annotations.E_FieldBehavior) {
 		return schema
 	}
-	fieldBehavior, ok := proto.GetExtension(dopts, annotations.E_FieldBehavior).(*annotations.FieldBehavior)
+	fieldBehaviors, ok := proto.GetExtension(dopts, annotations.E_FieldBehavior).([]annotations.FieldBehavior)
 	if !ok {
 		return schema
 	}
-	fb := fieldBehavior.Enum()
-	if fb == nil {
-		return schema
-	}
-	switch *fb {
-	case annotations.FieldBehavior_FIELD_BEHAVIOR_UNSPECIFIED:
-	case annotations.FieldBehavior_OPTIONAL:
-		schema.Description = "(OPTIONAL) " + schema.Description
-	case annotations.FieldBehavior_REQUIRED:
-		schema.ParentProxy.Schema().Required = util.AppendStringDedupe(schema.ParentProxy.Schema().Required, string(desc.Name()))
-	case annotations.FieldBehavior_OUTPUT_ONLY:
-		schema.ReadOnly = util.BoolPtr(true)
-	case annotations.FieldBehavior_INPUT_ONLY:
-		schema.WriteOnly = util.BoolPtr(true)
-	case annotations.FieldBehavior_IMMUTABLE:
-		schema.Description = "(IMMUTABLE) " + schema.Description
-	case annotations.FieldBehavior_UNORDERED_LIST:
-		schema.Description = "(UNORDERED_LIST) " + schema.Description
-	case annotations.FieldBehavior_NON_EMPTY_DEFAULT:
-		schema.Description = "(NON_EMPTY_DEFAULT) " + schema.Description
-	case annotations.FieldBehavior_IDENTIFIER:
-		schema.Description = "(IDENTIFIER) " + schema.Description
-	default:
+	for _, fieldBehavior := range fieldBehaviors {
+		fb := fieldBehavior.Enum()
+		if fb == nil {
+			return schema
+		}
+		switch *fb {
+		case annotations.FieldBehavior_FIELD_BEHAVIOR_UNSPECIFIED:
+		case annotations.FieldBehavior_OPTIONAL:
+			schema.Description = "(OPTIONAL) " + schema.Description
+		case annotations.FieldBehavior_REQUIRED:
+			schema.ParentProxy.Schema().Required = util.AppendStringDedupe(schema.ParentProxy.Schema().Required, string(desc.Name()))
+		case annotations.FieldBehavior_OUTPUT_ONLY:
+			schema.ReadOnly = util.BoolPtr(true)
+		case annotations.FieldBehavior_INPUT_ONLY:
+			schema.WriteOnly = util.BoolPtr(true)
+		case annotations.FieldBehavior_IMMUTABLE:
+			schema.Description = "(IMMUTABLE) " + schema.Description
+		case annotations.FieldBehavior_UNORDERED_LIST:
+			schema.Description = "(UNORDERED_LIST) " + schema.Description
+		case annotations.FieldBehavior_NON_EMPTY_DEFAULT:
+			schema.Description = "(NON_EMPTY_DEFAULT) " + schema.Description
+		case annotations.FieldBehavior_IDENTIFIER:
+			schema.Description = "(IDENTIFIER) " + schema.Description
+		default:
+		}
 	}
 	return schema
 }
