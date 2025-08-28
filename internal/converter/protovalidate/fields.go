567fefd75b134399f5590c06af23db014f98cac9
Ollie Mignot <olliemignot@gmail.com>
2025-08-29 06:01:25 +1000

    fix: buf validation rules no longer show in description if analogous to openapi validation (#134)

---
+++ internal/converter/protovalidate/fields.go
@@ -0,0 +1,19 @@
+package protovalidate
+
+const (
+	FieldConst       = "Const"
+	FieldIn          = "In"
+	FieldNotIn       = "NotIn"
+	FieldLessThan    = "LessThan"
+	FieldGreaterThan = "GreaterThan"
+	FieldExample     = "Example"
+	FieldLen         = "Len"
+	FieldMinLen      = "MinLen"
+	FieldMaxLen      = "MaxLen"
+	FieldPattern     = "Pattern"
+	FieldUnique      = "Unique"
+	FieldMinItems    = "MinItems"
+	FieldMaxItems    = "MaxItems"
+	FieldMinPairs    = "MinPairs"
+	FieldMaxPairs    = "MaxPairs"
+)
