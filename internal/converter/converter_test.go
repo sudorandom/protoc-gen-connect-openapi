cfeceddc3dd258c186ee3e64c22cb28705c8b3f6
Kevin <kevin@sudorandom.com>
2025-12-25 11:23:35 +0100

    chore: split up test cases

--- internal/converter/converter_test.go
+++ internal/converter/converter_test.go
@@ -178,13 +178,13 @@ func TestConvert(t *testing.T) {
 				formats := []string{"yaml", "json"}
 				for _, format := range formats {
 					t.Run(path.Base(protofile)+"→"+format, func(t *testing.T) {
-						if strings.Contains(protofile, "novalidate") {
-							t.Log("Skipping validation for novalidate test case")
-							return
-						}
 						spec := generateAndCheckResult(t, scenario.Options, format, protofile)
 						// Validate
 						t.Run("validate", func(tt *testing.T) {
+							if strings.Contains(protofile, "novalidate") {
+								tt.SkipNow()
+								return
+							}
 							validateOpenAPISpec(t, protofile, spec)
 						})
 					})
